3ceccb1bfe73e35747f658ea26fd7482554b6cf9
xiraynedev <xiraynedev@gmail.com>
2022-08-30 17:52:30 -0400

    add placeholder text for search field

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,5 +6,13 @@ class SearchForm(forms.Form):
     """search form class to convert into html form"""
 
     SEARCH_CHOICES = (("1", "title"), ("2", "contributor"))
-    search = forms.CharField(required=False, min_length=3)
+    search = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "title or contributor.",
+            }
+        ),
+        required=False,
+        min_length=3,
+    )
     search_in = forms.ChoiceField(choices=SEARCH_CHOICES, required=False)
